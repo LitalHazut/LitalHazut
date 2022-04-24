db5354e09c15bca19b54dd4017fb6abee323e906
lital hazut <lital7h@gmail.com>
2022-04-24 18:58:28 +0300

    add cache

--- core/__init__.py
+++ core/__init__.py
@@ -1,12 +1,18 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from decouple import config
+from flask_caching import Cache
 
 app = Flask(__name__)
 app.config.from_object(config("APP_SETTINGS"))
+
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
+cache.init_app(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from core import routes
+
