e09da52a618af33d927c0bcfeb4665a2ab1868cd
lital hazot <lital7h@gmail.com>
2022-04-28 17:57:48 +0300

    remove comments from code

--- core/__init__.py
+++ core/__init__.py
@@ -3,13 +3,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from decouple import config
-# from flask_caching import Cache
 
 app = Flask(__name__)
 app.config.from_object(config("APP_SETTINGS"))
-
-# cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
-# cache.init_app(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
