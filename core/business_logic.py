0bb9974e0b0d4b09aa75f43de8392a519d61948b
lital hazot <lital7h@gmail.com>
2022-05-03 13:50:55 +0300

    fix counters service

--- core/business_logic.py
+++ core/business_logic.py
@@ -5,15 +5,16 @@ from flask import render_template, request, flash, redirect, url_for
 import string
 from core.utils import generate_short_id
 
+
 def shorten_url(url: string, short_id: string):
     if is_valid_args(url, short_id):
-        if is_shorten_url_not_exists(url, short_id):
-            return insert_shortern_url_to_db(url, short_id)
+        return insert_shortern_url_to_db(url, short_id)
     else:
         return redirect(url_for('index'))
 
+
 def is_valid_args(url: string, short_id: string):
-    if short_id and ShortUrls.query.filter_by(short_id=short_id).first() is not None:
+    if short_id and is_shorten_url_exits(short_id):
         flash('Please enter different custom id!')
         return False
     if not url:
@@ -21,8 +22,10 @@ def is_valid_args(url: string, short_id: string):
         return False
     return True
 
-def is_shorten_url_not_exists(url: string, short_id: string):
-    return ShortUrls.query.filter_by(short_id=short_id, original_url=url).first() is None
+
+def is_shorten_url_exits(short_id: string):
+    return get_link_by_short_id(short_id) is not None
+
 
 def insert_shortern_url_to_db(url: string, short_id: string):
     if not short_id:
@@ -34,5 +37,6 @@ def insert_shortern_url_to_db(url: string, short_id: string):
     short_url = request.host_url + short_id
     return render_template('index.html', short_url=short_url)
 
-def get_link_by_short_id(short_id:string):
-    ShortUrls.query.filter_by(short_id=short_id).first()
+
+def get_link_by_short_id(short_id: string):
+    return ShortUrls.query.filter_by(short_id=short_id).first()
